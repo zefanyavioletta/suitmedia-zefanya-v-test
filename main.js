356659be15721d8aa28a6be97ab1cd3db0ef5c70
zefanyavioletta <zefanyavioletta97@gmail.com>
2025-07-22 22:12:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -162,12 +162,26 @@ window.addEventListener('DOMContentLoaded', () => {
   loadPosts(savedState.page, savedState.sortOrder);
 });
 
-//untuk white line tombol navbar
+
 const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     navLinks.forEach(l => l.classList.remove('active'));
     link.classList.add('active');
+
+
+    localStorage.setItem('activeNav', link.getAttribute('href'));
   });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const lastActive = localStorage.getItem('activeNav');
+  if (lastActive) {
+    navLinks.forEach(link => {
+      if (link.getAttribute('href') === lastActive) {
+        link.classList.add('active');
+      }
+    });
+  }
 });
