213dae5912d5cf9c773206ef77450ea5a7b90635
zefanyavioletta <zefanyavioletta97@gmail.com>
2025-07-22 21:59:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -166,7 +166,9 @@ const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
   link.addEventListener('click', (e) => {
+    e.preventDefault();
     navLinks.forEach(l => l.classList.remove('active'));
     link.classList.add('active');
+    window.location.href = link.href;
   });
 });
